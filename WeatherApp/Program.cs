e6d6b1ad41efd3c7f6be2c94767771776d73059e
Alec Hancock <alec.m.hancock@gmail.com>
2023-04-10 15:08:06 -0500

    Assignment Complete

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+
+string key = File.ReadAllText("appsettings.json");
+string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+Console.WriteLine("////////////////////////////////////////////////////");
+
+Console.WriteLine("Welcome to the Weather Channel*\n" +
+    "Please enter your zipcode for your local forecast.");
+
+Console.WriteLine("////////////////////////////////////////////////////");
+
+var zipCode = Console.ReadLine();
+string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+
+Console.WriteLine();
+
+Console.WriteLine($"It is currently {WeatherApp.WeatherMap.GetTemp(apiCall)};");
+
+
+
+
---
+++ WeatherApp/WeatherMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WeatherApp;
+using Newtonsoft.Json.Linq;
+namespace WeatherApp
+{
+	public class WeatherMap
+	{
+		public WeatherMap()
+		{
+		}
+
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+        }
+    }
+}
+
